60495280717ca22dc99aa29b2707a6f0f51a75ad
Yam Zhang <yam.zhang@shopee.com>
2022-12-12 11:35:36 +0800

    update

--- server/test.js
+++ server/test.js
@@ -8,4 +8,24 @@ sharp('input.jpg')
     // containing a scaled and cropped version of input.jpg
   });
 
+const express = require('express')
+
+const app = express()
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({'limit': '1000kb'}));
+  
+
+app.post('/upload-multiple-files', (req, res) => {
+  console.log("upload-multiple-files")
+  res.end('Hello, World!')
+})
+  
+app.post('/', (req, res) => {
+  console.log("root")
+  res.end('Hello, World!')
+})
+
+app.listen(3000)
+
 
