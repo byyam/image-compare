5f83e43db76a454af3be5140931e5f4f677aa26a
Yam Zhang <yanzhang.scut@gmail.com>
2023-01-11 15:25:43 +0800

    Update vipscli_test.go

--- pkg/processcli/vipscli_test.go
+++ pkg/processcli/vipscli_test.go
@@ -4,13 +4,44 @@ import (
 	"testing"
 )
 
+const (
+	convertNum     = 10
+	convertQuality = 50
+)
+
 func Test_SaveWebp(t *testing.T) {
-	if err := SaveImage(&ProcessCtx{
-		TargetFormat: FormatWebp,
-		Quality:      50,
-		Input:        "/Users/zhangyan/Documents/images/color.jpg",
-	}); err != nil {
-		t.Fatalf("save image failed:%v", err)
+	for i := 0; i < convertNum; i++ {
+		if err := SaveImage(&ProcessCtx{
+			TargetFormat: FormatWebp,
+			Quality:      convertQuality,
+			Input:        "/go/src/bimg/testdata/test.jpg",
+		}); err != nil {
+			t.Fatalf("save image failed:%v", err)
+		}
+	}
+}
+
+func Test_SaveAvif(t *testing.T) {
+	for i := 0; i < convertNum; i++ {
+		if err := SaveImage(&ProcessCtx{
+			TargetFormat: FormatAvif,
+			Quality:      convertQuality,
+			Input:        "/go/src/bimg/testdata/test.jpg",
+		}); err != nil {
+			t.Fatalf("save image failed:%v", err)
+		}
+	}
+}
+
+func Test_SaveHeif(t *testing.T) {
+	for i := 0; i < convertNum; i++ {
+		if err := SaveImage(&ProcessCtx{
+			TargetFormat: FormatHeif,
+			Quality:      convertQuality,
+			Input:        "/go/src/bimg/testdata/test.jpg",
+		}); err != nil {
+			t.Fatalf("save image failed:%v", err)
+		}
 	}
 }
 
@@ -27,8 +58,8 @@ func benchmarkGenerate(i int, format FormatType, b *testing.B) {
 func generate(n int, format FormatType) {
 	ctx := &ProcessCtx{
 		TargetFormat: format,
-		Quality:      50,
-		Input:        "/Users/zhangyan/Documents/images/color.jpg",
+		Quality:      convertQuality,
+		Input:        "/go/src/bimg/testdata/test.jpg",
 	}
 	for i := 0; i < n; i++ {
 		_ = SaveImage(ctx)
